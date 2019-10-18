34c3828fac32ea2dab399a29ee40c8dee2bfd3d9
Frank Martin <cedricmrtn@gmail.com>
2019-10-18 14:48:02 -0500

    finish jthw ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main( String[] args ) {
+    System.out.println( "+-----------------------+" );
+    System.out.println( "|                       |" );
+    System.out.println( "|      CORNER STORE     |" );
+    System.out.println( "|                       |" );
+    System.out.println( "| 2019-10-18  12:57PM   |" );
+    System.out.println( "|                       |" );
+    System.out.println( "| Gallons:       10.870 |" );
+    System.out.println( "| Price/gallons: $22.71 |" );
+    System.out.println( "|                       |" );
+    System.out.println( "| Fuel total:    $22.71 |" );
+    System.out.println( "|                       |" );
+    System.out.println( "+-----------------------+" );
+  }
+}
