7fe4df0662ff9805ed346b91206cd408a309414e
Frank Martin <cedricmrtn@gmail.com>
2019-10-18 16:26:44 -0500

    Completed Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+    System.out.println("Name : Frank Martin");
+    System.out.println("Age : 23");
+    System.out.println("State : Texas");
+  }
+}
