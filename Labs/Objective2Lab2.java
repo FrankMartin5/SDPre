403a917c20b289ff51ad2a0c5f29e72c194f7487
Frank Martin <cedricmrtn@gmail.com>
2019-10-19 11:37:39 -0500

    Completed Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Frank Martin";
+    int age = 23;
+    String state = "Kansas";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
